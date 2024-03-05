9faf1e9880416edde8b9381ed2c1d22db905193b
leilakhazma1 <leilakhazma7@gmail.com>
2024-03-05 16:33:15 +1100

    javascripttttt done i think

--- calc.js
+++ calc.js
@@ -54,9 +54,51 @@ function numberClick(number) {
 
 //Function to handle operation button clicks 
 
+function operationClick(operator) {
+  if (currentOperation !== null) {
+    secondOperand = currentInput; 
+    currentInput = operate(currentOperation, firstOperand, secondOperand).toString();
+    updateDisplay(currentInput);
+  }
 
+curentOperation = operator; 
+firstOperand = currentInput; 
+currentInput = '';
+}
 
-
-//Function to handle equals button clik 
+//Function to handle equals button click
+function equalsClick() {
+  if (currentOperation !== null) {
+    secondOperand = currentInput;
+    currentInput = operate(currentOperation, firstOperand, secondOperand).toString();
+    updateDisplay(currentInput);
+    currentOperation = null;
+    firstOperand = null;
+    secondOperand = null;
+  }
+}
 
 //Events for number buttons 
+
+document.getElementById('clear').addEventListener('click', clear);
+document.getElementById('divide').addEventListener('click', () => operationClick('/'));
+document.getElementById('multiply').addEventListener('click', () => operationClick('*'));
+document.getElementById('subtract').addEventListener('click', () => operationClick('-'));
+document.getElementById('add').addEventListener('click', () => operationClick('+'));
+document.getElementById('equals').addEventListener('click', equalsClick);
+document.getElementById('decimal').addEventListener('click', () => {
+  if (!currentInput.includes('.')) {
+    currentInput += '.';
+    updateDisplay(currentInput);
+  }
+});
+document.getElementById('zero').addEventListener('click', () => numberClick('0'));
+document.getElementById('one').addEventListener('click', () => numberClick('1'));
+document.getElementById('two').addEventListener('click', () => numberClick('2'));
+document.getElementById('three').addEventListener('click', () => numberClick('3'));
+document.getElementById('four').addEventListener('click', () => numberClick('4'));
+document.getElementById('five').addEventListener('click', () => numberClick('5'));
+document.getElementById('six').addEventListener('click', () => numberClick('6'));
+document.getElementById('seven').addEventListener('click', () => numberClick('7'));
+document.getElementById('eight').addEventListener('click', () => numberClick('8'));
+document.getElementById('nine').addEventListener('click', () => numberClick('9'));
